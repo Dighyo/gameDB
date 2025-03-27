7b2dd409a1ac621aefa52be48e6e53160ca85c23
Dighyo Colq <dwighyojcolquhoun@gmail.com>
2025-03-27 08:38:05 -0400

    Displaying critic score

--- src/components/GameCards.tsx
+++ src/components/GameCards.tsx
@@ -1,6 +1,7 @@
 import { Game } from "@/hooks/useGames";
-import { Card, Image } from "@chakra-ui/react";
+import { Card, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -13,9 +14,12 @@ const GameCards = ({ game }: Props) => {
       <Card.Body>
         <Card.Title fontSize="2xl">{game.name}</Card.Title>
         <Card.Description>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
         </Card.Description>
       </Card.Body>
     </Card.Root>
