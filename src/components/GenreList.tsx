2b5770cfbed825fba66dac485968301cd9d9aa88
Dighyo Colq <dwighyojcolquhoun@gmail.com>
2025-03-28 11:31:01 -0400

    Genre list fetching error handling

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,6 +9,9 @@ const GenreList = () => {
   const items = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
   ];
+
+  if (error) return <Text>An error ocurred</Text>;
+
   if (isloading)
     return (
       <>
@@ -20,7 +23,6 @@ const GenreList = () => {
 
   return (
     <>
-      {error && <Text>{error}</Text>}
       <List.Root variant="plain">
         {data.map((genre) => (
           <List.Item key={genre.id} paddingY="5px">
