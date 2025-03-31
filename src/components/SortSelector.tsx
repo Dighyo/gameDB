04d64b6a660f9983a80d558b1fcc09bc99f29d7a
Dighyo Colq <dwighyojcolquhoun@gmail.com>
2025-03-31 10:39:45 -0400

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,23 +1,43 @@
 import { Menu, Button, Portal } from "@chakra-ui/react";
 import { FaChevronDown } from "react-icons/fa";
 
-const SortSelector = () => {
+interface Props {
+  onSelectedSortOrder: (sortOrder: string) => void;
+  sortOrderValue: string;
+}
+
+const SortSelector = ({ onSelectedSortOrder, sortOrderValue }: Props) => {
+  const sortOrder = [
+    { value: "", label: "Relevance" },
+    { value: "added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentSortOrder = sortOrder.find(
+    (order) => order.value === sortOrderValue
+  );
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="outline" size="lg">
-          Order by: Relevance <FaChevronDown />{" "}
+          Order by: {currentSortOrder?.label || "Relevance"} <FaChevronDown />{" "}
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            <Menu.Item value="relevance">Relevance</Menu.Item>
-            <Menu.Item value="date-added">Date Added</Menu.Item>
-            <Menu.Item value="name">Name</Menu.Item>
-            <Menu.Item value="release-date">Release Date</Menu.Item>
-            <Menu.Item value="popularity">Popularity</Menu.Item>
-            <Menu.Item value="average-rating">Average Rating</Menu.Item>
+            {sortOrder.map((order) => (
+              <Menu.Item
+                onClick={() => onSelectedSortOrder(order.value)}
+                key={order.value}
+                value={order.value}
+              >
+                {order.label}
+              </Menu.Item>
+            ))}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
