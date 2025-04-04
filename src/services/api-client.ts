e349766eba7c52b2a2c7e62cb7be39fe13628bbc
Dighyo Colq <dwighyojcolquhoun@gmail.com>
2025-04-04 09:44:43 -0400

    Store key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "",
+    key: supabaseKey,
   },
 });
